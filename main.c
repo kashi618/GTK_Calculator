53fff95b19470c7df5f8b1ac40b4bd817891b846
kashi <nialj618@gmail.com>
2026-02-12 14:04:42 +0000

    In process of converting global variables to structs

--- main.c
+++ main.c
@@ -1,21 +1,67 @@
 #include <gtk/gtk.h>
 
-#define BUTTON_SPACING 5
-#define BUTTON_SIZE 80
-#define MAX_EQUATION 100 // Maximum amount of operators and digits
 
+/* Macros */
+#define BUTTON_SPACING 5  // Sets spacing between buttons (try to keep it at 5)
+#define BUTTON_SIZE 80    // Sets size of buttons
+#define MAX_EQUATION 100  // Maximum amount of operators and digits
+
+
+/* Function Signatures */
 static void activate(GApplication *, gpointer);
 static void button_setup(GtkWidget *);
 static void create_button(GtkWidget *, char *, int, int, int);
 static void button_clicked(GtkWidget *, gpointer);
-static void calculate();
+static void calculate(int [], int);
 
 static int  is_operator(char);
 static int  is_empty(char);
 
-char calcEq[MAX_EQUATION] = {"\0"};
-int calcIndex = 0; // 0 means empty. 1 means there is one element stored at index=0
+static int* get_digits(int *, int);
+//static int* get_operators();
 
+
+/* Structs */
+typedef struct {
+    // String of current equation
+    char eqStr[MAX_EQUATION];
+    int eqStrIdx = 0;
+    
+    // List of digits in current equation
+    int* digits;
+    int digitsIdx = 0;
+    
+    // list of operators in current equation
+    char* operators;
+    int operatorsIdx = 0;
+
+    // Stores if current equation has an operator
+    int hasOperator;
+    int hasOperatorIdx = 0;
+    
+    // Result of calculation
+    double result;
+} CalculatorState;
+
+
+/* Global variables */
+CalculatorState calc;
+
+char calcEq[MAX_EQUATION] = {"\0"};
+int  calcIndex = 0;
+
+int  containsOperator = 0;
+int  operatorIndex = 0;
+
+
+
+/*
+int listDigits[MAX_EQUATION];    // List of digits in the current equation
+int listOperators[MAX_EQUATION]; // List of operators in the current equation
+int listOperatorsIndex = 0, listDigitsIndex = 0;
+*/
+
+// Main
 int main(int argc, char* argv[]) {
     // Setup GTK
     GtkApplication *app;
@@ -31,9 +77,10 @@ int main(int argc, char* argv[]) {
 }
 
 
-// Starts Calculator
+/*
+*  Starts calculator window 
+*/
 static void activate(GApplication *app, gpointer user_data) {
-    int i;
     GtkWidget *win;
 
     // Window setup and properties
@@ -58,7 +105,9 @@ static void activate(GApplication *app, gpointer user_data) {
 } // END activate
 
 
-// Set up Calculator buttons
+/*
+*  Sets up calculator button layout and functionality
+*/
 static void button_setup(GtkWidget *grid) {
     GtkWidget *button;
     int row, col;
@@ -81,7 +130,7 @@ static void button_setup(GtkWidget *grid) {
             gtk_widget_set_size_request(button, BUTTON_SIZE, BUTTON_SIZE);
             g_signal_connect(button, "clicked", G_CALLBACK(button_clicked), NULL);
 
-            // Double grid Keys (0 and =)
+            // Set size of double grid Keys (0 and =)
             if (row==3 && (col==0 || col==3)) {
                 gtk_grid_attach(GTK_GRID(grid), button, col, row, 2, 1);
             }
@@ -97,7 +146,7 @@ static void button_setup(GtkWidget *grid) {
                 gtk_grid_attach(GTK_GRID(grid), button, col, row, 1, 1);
             }
 
-            // Single grid keys (digits, operators, decimal)
+            // Regular/single grid keys (digits, operators, decimal)
             else {
                 gtk_grid_attach(GTK_GRID(grid), button, col, row, 1, 1);
             }
@@ -106,22 +155,37 @@ static void button_setup(GtkWidget *grid) {
 } // END button_setup
 
 
+/*
+*  Call back function for when a button is pressed
+*/
 static void button_clicked(GtkWidget *button, gpointer user_data) {
     const char *label = gtk_button_get_label(GTK_BUTTON(button));
+    
     switch (label[0]) {
         case '=':
-            if (is_operator(calcEq[calcIndex-1])) {
+            if (has_operator(calc) && is_operator(calc.eqStr[eqStrIdx-1])) {
                 printf("Cannot end with operator\n");
                 break;
             }
 
-            calculate();
+            if (containsOperator == 0) {
+                printf("Must contain atleast one operator\n");
+                break;
+            }
+
+            //calculate(calcEq, calcIndex);
             break;
 
         case 'D':
             if (calcIndex>0) {
-                printf("Deleted %c\n", calcEq[--calcIndex]);
+                calcIndex--; // Due to string terminator
+                printf("Deleted %c\n", calcEq[calcIndex]);                
                 calcEq[calcIndex] = '\0';
+            }
+            else if (containsOperator == 1) {
+                if (calcIndex == operatorIndex) {
+                    containsOperator = 0;
+                }
             }
             else {
                 printf("No operations\n");
@@ -132,7 +196,7 @@ static void button_clicked(GtkWidget *button, gpointer user_data) {
             printf("AC!\n");
 
             calcEq[0] = '\0';
-            calcIndex = -1;
+            calcIndex = 0;
             break;
 
         default:
@@ -148,22 +212,32 @@ static void button_clicked(GtkWidget *button, gpointer user_data) {
                 break;
             }
 
+            // Updates when equation has an operator
+            if (is_operator(label[0])) containsOperator=1;
+
+
             // Increment calcIndex, and set it to input
-            calcEq[calcIndex] = label[0];
-            calcEq[++calcIndex] = '\0';
+            calcEq[calcIndex++] = label[0];
+            calcEq[calcIndex] = '\0';
     }
 
     if (label[0] != '=') printf("Current equation: %s\n", calcEq);
 }
 
-static void calculate() {
-    int i;
-    
+
+/*
+*  Calculates a string equation
+*/
+static void calculate(int calcEq[], int calcIndex) {
     printf("Calculating.........\n");
+
+    int *digit = get_digits(calcEq, calcIndex);
 }
 
 
-// Returns 1 if operator
+/*
+*  Returns True (1) if a character is an operator
+*/
 static int is_operator(char x) {
     switch (x) {
         case '+': case '-': case 'x': case '/':
@@ -171,5 +245,39 @@ static int is_operator(char x) {
             break;
         default:
             return 0;
+    }
+}
+
+
+/*
+*  Returns array of digits from a string equation 
+*/
+static int* get_digits(int* calcEq, int calcIndex) {
+    int count = 0;
+    
+    // Count how many digits there are
+    for (int i=0; i<calcIndex; i++) {
+        if (!is_operator(calcEq[i])) {
+            count++;
+        }
+    }
+
+    printf("Amount digits: %d\n", count);
+    //int listDigit
+    //return listDigit;
+}
+
+
+
+/*
+static char* get_operators() {
+
+    
+}
+*/
+
+static int has_operator(CalculatorState calc) {
+    if (calc.operatorIdx) {
+         
     }
 }
